beade12081f172ebaa8dfcb365a563d9d44a622c
Lidya Tsegay <lidyatsegay@outlook.com>
2019-09-03 10:28:42 -0400

    initial commit

--- GeometricShapes/Program.cs
+++ GeometricShapes/Program.cs
@@ -1,9 +1,18 @@
-﻿using System;
+﻿using GeometricShapesLibrary;
+using System;
 
 namespace GeometricShapes {
     class Program {
+
+        void Run() {
+            var quad1 = new Quadrilateral(2,3,4,5);
+            Console.WriteLine($"quad1: perimeter is {quad1.Perimeter()}");
+
+        }
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var pgm = new Program();
+            pgm.Run();
         }
     }
 }
---
+++ GeometricShapesLibrary/Quad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometricShapesLibrary {
+    public class Quadrilateral {
+
+        public int Side1 { get; set; }
+        public int Side2 { get; set; }
+        public int Side3 { get; set; }
+        public int Side4 { get; set; }
+
+        //method to return the calculated perameter
+        public int Perimeter() {
+            return Side1 + Side2 + Side3 + Side4;
+        }
+
+        public Quadrilateral(int side1, int side2, int side3, int side4) { //Constructor
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+            Side4 = side4;
+
+        }
+    }
+}
